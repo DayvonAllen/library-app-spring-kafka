a17a4479478d158f0a0f35ba59829c586cda1fec
DayvonAllen <dayvonallen93@outlook.com>
2021-05-01 19:49:29 -0500

    configuration of kafka listener container

--- library-events-consumer/src/main/java/com/example/libraryeventsconsumer/config/LibraryEventsManualConsumerConfig.java
+++ library-events-consumer/src/main/java/com/example/libraryeventsconsumer/config/LibraryEventsManualConsumerConfig.java
@@ -1,27 +1,78 @@
-//package com.example.libraryeventsconsumer.config;
+package com.example.libraryeventsconsumer.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.RecoverableDataAccessException;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.retry.RetryPolicy;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+@Slf4j
+public class LibraryEventsManualConsumerConfig {
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(ConcurrentKafkaListenerContainerFactoryConfigurer configurer, ConsumerFactory<Object, Object> kafkaConsumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory();
+        configurer.configure(factory, kafkaConsumerFactory);
+
+        // specifies that we will run 3 instances of this listener(3 threads), only need this if
+        // you are not running your application in a cloud environment or not using kubernetes
+        factory.setConcurrency(3);
+
+        //custom error handling
+        factory.setErrorHandler(((thrownException, data) -> {
+            log.info("Exception in consumer config: {}, record: {}", thrownException.getMessage(), data);
+        }));
+
+        //add retries
+
+        factory.setRetryTemplate(retryTemplate());
+
+        // override AckMode from batch  to Manual
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        return factory;
+    }
+
+    private RetryTemplate retryTemplate() {
+
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+
+        // 1000 milliseconds
+        fixedBackOffPolicy.setBackOffPeriod(1000);
+
+        RetryTemplate retryTemplate = new RetryTemplate();
+        retryTemplate.setRetryPolicy(simpleRetryPolicy());
+        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+
+        return retryTemplate;
+    }
+
+    private RetryPolicy simpleRetryPolicy() {
+//        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
 //
-//import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.kafka.annotation.EnableKafka;
-//import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-//import org.springframework.kafka.core.ConsumerFactory;
-//import org.springframework.kafka.listener.ContainerProperties;
-//
-//@Configuration
-//@EnableKafka
-//public class LibraryEventsManualConsumerConfig {
-//    @Bean
-//    ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(ConcurrentKafkaListenerContainerFactoryConfigurer configurer, ConsumerFactory<Object, Object> kafkaConsumerFactory) {
-//        ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory();
-//        configurer.configure(factory, kafkaConsumerFactory);
-//
-//        // specifies that we will run 3 instances of this listener(3 threads), only need this if
-//        // you are not running your application in a cloud environment or not using kubernetes
-//        factory.setConcurrency(3);
-//
-//        // override AckMode from batch  to Manual
-//        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
-//        return factory;
-//    }
-//}
+//        simpleRetryPolicy.setMaxAttempts(3);
+
+        Map<Class<? extends  Throwable>, Boolean> exceptionsMap = new HashMap<>();
+
+        // if false you don't want to retry, if true you want to retry
+        exceptionsMap.put(IllegalArgumentException.class, false);
+        exceptionsMap.put(RecoverableDataAccessException.class, true);
+
+        // num of retries map of exceptions that you want to retry on, traverseCause
+        return new SimpleRetryPolicy(3, exceptionsMap, true);
+    }
+}
+
+
+
